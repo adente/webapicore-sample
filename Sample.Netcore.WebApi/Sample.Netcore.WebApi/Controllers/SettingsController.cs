33ebdab9e2bb24698d00b47dacc398c98356fa73
Antonio Dente <andente@microsoft.com>
2018-09-17 15:48:28 +0100

    adding settings controller;

---
+++ Sample.Netcore.WebApi/Sample.Netcore.WebApi/Controllers/SettingsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample.Netcore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public SettingsController(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        // GET: api/Settings
+        [HttpGet]
+        public string Get()
+        {
+            var result =  Configuration["SampleSetting"];
+            return result;
+        }
+
+        // GET: api/Settings/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(string id)
+        {
+            return "value";
+        }
+    }
+}
